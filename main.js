f7e4a274544167505908197a070be8d7edd13b6f
antonielisamira <162495519+antonielisamira@users.noreply.github.com>
2024-04-26 15:23:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,13 @@
-</section>
-<script src="main.js"></script>
-</body>
-</html>
-const botoes = document.querySelectorAll(".botao");
-const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
 const botoes = document.querySelectorAll(".botao");
 
-for()
-const botoes = document.querySelectorAll(".botao");
+for(let i=0;i ,botoes.length;i++){
+    botoes[i].onclick = function(){
 
-for(let i=0;)
-const botoes = document.querySelectorAll(".botao");
+        for(let j=0;j,botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
 
-for(let i=0;)
-const botoes = document.querySelectorAll(".botao");
-
-for(let i=0;i <botoes.length;i++){
-    console.log();
+        botoes[i].classList.add("ativo");
+    
+    }
 }
